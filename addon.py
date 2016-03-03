928f25149fbccc2c746875ee30df3ec4a25b8eb7
Damon Toumbourou <dtoumbourou@gmail.com>
2016-03-03 15:52:35 +1100

    ready for repo

--- addon.py
+++ addon.py
@@ -1,8 +1,7 @@
 from xbmcswift2 import Plugin, xbmcgui
-from resources.lib import thisweekscraper
+from resources.lib import buzzfeedscraper
 
 PLUGIN_URL = 'plugin://plugin.video.youtube/?action=play_video&videoid'
-SITE_URL = 'https://www.youtube.com/user/ThisWeekIn'
 
 plugin = Plugin()
 
@@ -12,66 +11,44 @@ def main_menu():
 
     items = [
         {
+            # Buzz Feed Video
             'label': plugin.get_string(30000),
-            'path': plugin.url_for('get_latest'),
-            'thumbnail': 'http://ec-cdn-assets.stitcher.com/feedimagesplain328/9728.jpg',
+            'path': plugin.url_for('get_content', url='https://www.youtube.com/user/BuzzFeedVideo/videos'),
+            'thumbnail': 'https://yt3.ggpht.com/-iJJcDIkIjL0/AAAAAAAAAAI/AAAAAAAAAAA/Rf6VBJ2D-MA/s900-c-k-no/photo.jpg',
         },
         {
+            # Buzz Feed Food
             'label': plugin.get_string(30001),
-            'path': plugin.url_for('get_highlight'),
-            'thumbnail': 'http://ec-cdn-assets.stitcher.com/feedimagesplain328/9728.jpg',
+            'path': plugin.url_for('get_content', url='https://www.youtube.com/user/BuzzFeedFood/videos'),
+            'thumbnail': 'https://pbs.twimg.com/profile_images/519943742258552832/wmQhQR3V.png',
         },
         {
+            # Buzz Feed Yellow
             'label': plugin.get_string(30002),
-            'path': plugin.url_for('get_topten'),
-            'thumbnail': 'http://ec-cdn-assets.stitcher.com/feedimagesplain328/9728.jpg',
+            'path': plugin.url_for('get_content', url='https://www.youtube.com/user/BuzzFeedYellow/videos'),
+            'thumbnail': 'https://yt3.ggpht.com/-qx3h-z3lc9w/AAAAAAAAAAI/AAAAAAAAAAA/QKFUBQtVG6A/s900-c-k-no/photo.jpg',
+        },
+        {
+            # Buzz Feed Violet
+            'label': plugin.get_string(30003),
+            'path': plugin.url_for('get_content', url='https://www.youtube.com/user/buzzfeedviolet/videos'),
+            'thumbnail': 'https://yt3.ggpht.com/--cdWOKrFOOE/AAAAAAAAAAI/AAAAAAAAAAA/wzHpOOWJAb4/s900-c-k-no/photo.jpg',
+        },
+        {
+            # Buzz Feed Blue
+            'label': plugin.get_string(30004),
+            'path': plugin.url_for('get_content', url='https://www.youtube.com/user/buzzfeedblue/videos'),
+            'thumbnail': 'https://yt3.ggpht.com/-GpCZ25vJ6CU/AAAAAAAAAAI/AAAAAAAAAAA/6qGF_ZTSSf0/s900-c-k-no/photo.jpg',
         }
     ]
     
     return items
 
 
-@plugin.route('/latest/')
-def get_latest():
+@plugin.route('/content/<url>')
+def get_content(url):
     
-    keyword = 'Uploads' 
-    content = thisweekscraper.get_latest(SITE_URL, keyword)
-    items = []
-
-    for i in content:
-        items.append({
-            'label': i['label'],
-            'path': PLUGIN_URL + i['path'],
-            'thumbnail': i['thumbnail'],
-            'is_playable': True,
-        })
-
-    return items
-
-
-@plugin.route('/highlight/')
-def get_highlight():
-    
-    keyword = 'Highlight Clips' 
-    content = thisweekscraper.get_latest(SITE_URL, keyword)
-    items = []
-
-    for i in content:
-        items.append({
-            'label': i['label'],
-            'path': PLUGIN_URL + i['path'],
-            'thumbnail': i['thumbnail'],
-            'is_playable': True,
-        })
-
-    return items
-
-
-@plugin.route('/topten/')
-def get_topten():
-    
-    keyword = 'Top 10 TWiST videos' 
-    content = thisweekscraper.get_latest(SITE_URL, keyword)
+    content = buzzfeedscraper.get_latest(url)
     items = []
 
     for i in content:
